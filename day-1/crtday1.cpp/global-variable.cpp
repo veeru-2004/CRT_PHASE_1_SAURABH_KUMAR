35e3c0e0e5515389f2684a144b2b8ac03ebeae93
veeru-2004 <veerupatel82004@gmail.com>
2026-05-02 11:54:30 +0530

    Update global-variable.cpp

--- day-1/crtday1.cpp/global-variable.cpp
+++ day-1/crtday1.cpp/global-variable.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-// Global variable
+
 int num = 200;
 
-// Function
+
 void divide() {
     int userInput;
     double result;
@@ -12,7 +12,7 @@ void divide() {
     cout << "Enter a number: ";
     cin >> userInput;
 
-    // Explicit type casting
+    
     result = (double) num / userInput;
 
     cout << "Result is: " << result << endl;
