0dc33adc62dece583240cddd690f3149523cb043
veeru-2004 <veerupatel82004@gmail.com>
2026-05-04 11:08:26 +0530

    removed comment

--- day-3/day-4/reverse-tri.cpp
+++ day-3/day-4/reverse-tri.cpp
@@ -5,18 +5,12 @@ int main() {
     int n = 5;
 
     for(int i = 1; i <= n; i++) {
-
-        // 1. Print spaces
         for(int j = 1; j <= n - i; j++) {
             cout << "  ";
         }
-
-        // 2. Print stars
         for(int j = 1; j <= i; j++) {
             cout << "* ";
         }
-
-        // Move to next line
         cout << endl;
     }
 
