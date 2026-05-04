fc6a17117c55143ba1052c1f16fa18bae63e111e
veeru-2004 <veerupatel82004@gmail.com>
2026-05-04 09:52:07 +0530

    Added star matrix program (day 4)

---
+++ day-3/day-4/for-star.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ int main (){
+    int a,b;
+  cout<< " enter the value of matrix a and b: "<<endl;
+
+  cin>> a >>b ;
+
+  for( int i = 1 ; i <=a ; i++ ){
+  for( int j = 1 ; j<=b ; j++){
+    cout<< "*";
+
+  }
+    cout << endl ;
+
+  }
+return 0;
+
+ }
